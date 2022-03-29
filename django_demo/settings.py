9dc3583855ce5ae79d8c4a77b7e04926328915e4
Smr341 <smriti.misra9@gmail.com>
2022-03-29 12:16:03 +0530

    debug

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-om@_euwi#*_t!ags^$x^d$#ejnrf(_uyj8)uoraycx9r@bppsk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
